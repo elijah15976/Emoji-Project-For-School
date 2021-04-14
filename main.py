a4dfc8b98705c60b63f04f9fcf21630c5e0c8fe0
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-13 20:56:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from turtle import *
+import turtle
 
 bob = turtle.Turtle()
+
+
+def polygon(tut, side, len, lr):
+  for i in range(side)
+    tut.forward(len)
+    if lr == left:
+      tut.left(360/side)
+    elif lr == right:
+      tut.right(360/side)
