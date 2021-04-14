40a5ddb7f3bcb24db6ddf0b8c8b3bd6580ed7453
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-14 07:25:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from core_functions import *
 
 bob = turtle.Turtle()
 
-polygon(bob, 4, 100, "right")
+bob.color(0, 100, 200)
+bob.begin_fill()
+polygon(bob, 4, 300, "right")
+bob.end_fill()
 
 
 #Put this at end of code
-time.sleep( 5 )
+time.sleep(5)
