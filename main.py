95f161dfb915d1988a2e6f24734b9cf6e8e10462
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-14 07:50:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ bob.begin_fill()
 polygon(bob, 4, 300, "right")
 bob.end_fill()
 
+jump(bob, 50, -50)
+
+bob.color("#3F0000")
+bob.begin_fill()
+rect(bob, 25, 50, "right")
+bob.end_fill()
+
+jump(bob, 225, -50)
+
+bob.color("#3F0000")
+bob.begin_fill()
+rect(bob, 25, 50, "right")
+bob.end_fill()
+
 
 #Put this at end of code
 time.sleep(5)
