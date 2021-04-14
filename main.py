40acf9a4f569368bceaed03b96a74a31221c2913
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-13 21:03:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ bob = turtle.Turtle()
 def polygon(tut, side, len, lr):
   for i in range(side):
     tut.forward(len)
-    if lr == left:
+    if lr == "left":
       tut.left(360/side)
-    elif lr == right:
+    elif lr == "right":
       tut.right(360/side)
 
 polygon(bob, 4, 100, "right")
