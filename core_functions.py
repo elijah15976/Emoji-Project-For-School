c5935c9d377fdb2bf55e2c85ec7acec5fcbd32a4
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-14 07:50:20 -0400

    Update core_functions.py

--- core_functions.py
+++ core_functions.py
@@ -10,3 +10,16 @@ def jump(tut, x, y):
   tut.penup()
   tut.goto(x, y)
   tut.pendown()
+
+def rect(tut, l, w, lr):
+  for i in range(2):
+    tut.forward(l)
+    if lr == "left":
+      tut.left(90)
+    elif lr == "right":
+      tut.right(90)
+    tut.forward(w)
+    if lr == "left":
+      tut.left(90)
+    elif lr == "right":
+      tut.right(90)
