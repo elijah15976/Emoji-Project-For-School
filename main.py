7837938e6a06f13ea5ec040f3c98c781482e0c0b
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-13 21:01:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 bob = turtle.Turtle()
 
@@ -10,3 +11,9 @@ def polygon(tut, side, len, lr):
       tut.left(360/side)
     elif lr == right:
       tut.right(360/side)
+
+polygon(bob, 4, 100, right)
+
+
+#Put this at end of code
+time.sleep( 5 )
