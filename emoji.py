08ebcc558af8966c29e1ec0d0c171f6bbcb61fe3
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-15 09:22:52 -0400

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -89,3 +89,45 @@ def Happy():
   bob.begin_fill()
   bob.circle(10)
   bob.end_fill()
+
+
+
+
+def Sad():
+  bob = turtle.Turtle()
+  bob.speed(0)
+
+  bob.color("#0077FF")
+
+  #Box head
+  jump(bob, -150, 150)
+  bob.begin_fill()
+  polygon(bob, 4, 300, "right")
+  bob.end_fill()
+
+  bob.color("#3F0000")
+  #Left Eye
+  jump(bob, -100, 100)
+  bob.begin_fill()
+  rect(bob, 25, 50, "right")
+  bob.end_fill()
+
+  #Right Eye
+  jump(bob, 75, 100)
+  bob.begin_fill()
+  rect(bob, 25, 50, "right")
+  bob.end_fill()
+
+  #Mouth
+  jump(bob, -50, -125)
+  bob.begin_fill()
+  polygon(bob, 3, 100, "left")
+  bob.end_fill()
+
+  bob.color("#0037FF")
+
+  #Nose
+  jump(bob, 0, 0)
+  bob.begin_fill()
+  bob.circle(10)
+  bob.end_fill()
