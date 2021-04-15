8ad192ed5c48f658fcb907ee3b4046b40a7d751d
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-15 09:22:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 from core_functions import *
 from emoji import *
 
-em = random.randint(1, 2)
+em = random.randint(1, 3)
 
 print("You Got:")
 
@@ -14,6 +14,12 @@ if em == 1:
 elif em == 2:
   print("HAPPY GUY")
   Happy()
+elif em == 3:
+  print("SAD GUY")
+  Sad()
+else:
+  print("Something went wrong, please try again.")
+  print("\nError: Num greater than 3 or less than 1")
 
 
 #Put this at end of code
